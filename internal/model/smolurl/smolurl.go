5943c1b0aafea49097ffc5385f5182204597470e
Mist <mistic0xb@gmail.com>
2026-03-10 18:45:18 +0530

    database interaction for smolurls

--- internal/model/smolurl/smolurl.go
+++ internal/model/smolurl/smolurl.go
@@ -2,14 +2,12 @@ package smolurl
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type SmolURL struct {
-	ID             uuid.UUID `json:"id" db:"id"`
-	OriginalURL    string    `json:"original_url" db:"original_url"`
-	ShortURL       string    `json:"short_url" db:"short_url"`
-	ExpirationTime time.Time `json:"expiration_time" db:"expiration_time"`
-	CreatedAt      time.Time `json:"created_at" db:"created_at"`
+	ID          uint64    `json:"id" db:"id"`
+	OriginalURL string    `json:"original_url" db:"original_url"`
+	SmolURL     string    `json:"smol_url" db:"smol_url"`
+	ExpiresAt   time.Time `json:"expires_at" db:"expires_at"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 }
--- internal/repository/smolurl.go
+++ internal/repository/smolurl.go
@@ -1,9 +1,60 @@
 package repository
 
-import "github.com/mistic0xb/smolurl/internal/server"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/mistic0xb/smolurl/internal/model/smolurl"
+	"github.com/mistic0xb/smolurl/internal/server"
+)
 
 type SmolURLRepository struct {
 	server *server.Server
 }
 
-func NewSmolURLRepository(s *server.Server) *SmolURLRepository { return &SmolURLRepository{} }
+func NewSmolURLRepository(server *server.Server) *SmolURLRepository {
+	return &SmolURLRepository{server: server}
+}
+
+func (r *SmolURLRepository) CreateSmolURL(ctx context.Context, payload *smolurl.SmolURL) (*smolurl.SmolURL, error) {
+	stmt := `
+		INSERT INTO 
+			smolurls (
+				id,
+				original_url,
+				smol_url,
+				expires_at,
+				created_at
+			)
+		VALUES
+			(
+				@id,
+				@original_url,
+				@smol_url,
+				@expires_at,
+				@created_at
+			)
+		RETURNING *
+	`
+	rows, err := r.server.DB.Pool.Query(ctx, stmt, pgx.NamedArgs{
+		"id":           payload.ID,
+		"original_url": payload.OriginalURL,
+		"smol_url":     payload.SmolURL,
+		"expires_at":   payload.ExpiresAt,
+		"created_at":   payload.CreatedAt,
+	})
+	if err != nil {
+		log.Fatal(err)
+		return nil, fmt.Errorf("failed to execute create smolurl query for id=%v original_url=%v: %v", payload.ID, payload.OriginalURL, err)
+	}
+
+	smolURLItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[smolurl.SmolURL])
+	if err != nil {
+		log.Fatal(err)
+		return nil, fmt.Errorf("failed to collect row from table:smolurls for id=%v original_url=%v: %v", payload.ID, payload.OriginalURL, err)
+	}
+
+	return &smolURLItem, nil
+}
